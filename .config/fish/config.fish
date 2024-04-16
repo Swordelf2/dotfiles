6068a6b5ac9ba6062f81842e64152615617925db
Yevgeny Papazyan <evgeny1998oo@gmail.com>
2024-04-16 13:43:43 +0300

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    starship init fish | source
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
     source "$XDG_CONFIG_HOME/shell/aliasrc"
