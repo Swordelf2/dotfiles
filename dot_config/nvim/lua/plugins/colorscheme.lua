5250b6b276d42f2e52dbf5cca9725b51d835d1f2
Yevgeny Papazyan <evgeny1998oo@gmail.com>
2026-08-31 17:04:27 +0300

    add nvim

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- Temp fix for `bufferline.get` issue
+  -- Issue: https://github.com/LazyVim/LazyVim/issues/6355
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
