75d3b750834c56deb3df792e5344df9b67b3619b
Yevgeny Papazyan <evgeny1998oo@gmail.com>
2026-09-15 19:34:33 +0300

    [codex-auto:work] Add copy file location keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,17 @@
 
 vim.keymap.set("n", "q", "<cmd>quit<cr>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Write" })
+
+local function copy_file_location()
+	local filepath = vim.fn.expand("%:p")
+	if filepath == "" then
+		vim.notify("Current buffer has no file path", vim.log.levels.WARN)
+		return
+	end
+
+	local location = string.format("%s:%d:%d", filepath, vim.fn.line("."), vim.fn.col("."))
+	vim.fn.setreg("+", location, "c")
+	vim.notify("Copied: " .. location)
+end
+
+vim.keymap.set("n", "<leader>fy", copy_file_location, { desc = "Copy File Location" })
